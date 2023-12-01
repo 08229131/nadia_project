04b18a41744b57b4136a58b8ab6933a83971bb41
fentris <152472773+fentris@users.noreply.github.com>
2023-12-01 11:15:40 +0800

    menambahkan tugas teknologi open source

--- tugas.php
+++ tugas.php
@@ -1,4 +1,6 @@
 TUGAS TEKNOLOGI OPEN SOURCE
 - mendaftar ke github.com
--membuat file baru
--edit README nya agar lebih informatif 
+- membuat file baru
+- edit README nya agar lebih informatif 
+- Tambahkan suatu file baru, simpan sebagai branch baru.
+- Merge branch baru tersebut ke master branch (pull request)
